9d414abad31d14c290ff9981f1291fb0e4b12c1a
Leonard Brinkworth <Leonard.Brinkworth@gmail.com>
2021-06-04 09:24:17 +0930

    me put light on mech.

---
+++ CrazyUnhingedManInNormalEnoughMech/Assets/Scripts/Leonard/WeaponHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponHolder : MonoBehaviour
+{
+    public Weapon weapon;
+    Image thisImage;
+    Player thePlayer;
+
+    void Start()
+    {
+        thisImage = GetComponent<Image>();
+        thePlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (thePlayer.currentWeapon.projectile == weapon.projectile)
+        {
+            thisImage.color = Color.red;
+        }
+        else
+        {
+            thisImage.color = Color.blue;
+        }
+    }
+}
